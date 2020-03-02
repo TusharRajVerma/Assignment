38cf1f0a92c1821a7f12d7768347b5d2028bb891
Tushar <tusharrajverma30@gmail.com>
2020-03-02 15:23:55 +0530

    done

--- Codes/Ans1.java
+++ Codes/Ans1.java
@@ -1,19 +1,21 @@
-package com.tothenew;
-
-//sleep() can be used to pause a thread
 public class Ans1 {
     public static void main(String[] args) {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("Hey am a thread , showing use of sleep()");
+                for (int i = 0; i < 10; i++) {
+                    try{
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    System.out.println("Hey am a thread , showing use of sleep()");
+                }
             }
         });
-        try{
-            thread.sleep(1000);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
+
         thread.start();
+	
     }
 }
+
